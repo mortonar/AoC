b21b1702b064f08625cdac8f1ebb876b989b9589
Alex Morton <mortonar01@gmail.com>
2025-04-17 09:04:16 -0400

    Day 4: small refactor

--- 2015/day4/src/main.rs
+++ 2015/day4/src/main.rs
@@ -8,17 +8,17 @@ fn main() -> Result<()> {
 
     // In the samples given, we appear to need at least n+1 digits where n is # of 0s in prefix.
     // This is a bit slow for part 2...
-    let num = 100000;
-    let p1 = find_secret(&input, num, 5).context("Part 1 not found")?;
+    let p1 = find_secret(&input, 5).context("Part 1 not found")?;
     println!("Part 1: {p1}");
-    let p2 = find_secret(&input, num << 1, 6).context("Part 2 not found")?;
+    let p2 = find_secret(&input, 6).context("Part 2 not found")?;
     println!("Part 2: {p2}");
 
     Ok(())
 }
 
-fn find_secret(input: &str, start: usize, prefix_size: usize) -> Option<usize> {
+fn find_secret(input: &str, prefix_size: usize) -> Option<usize> {
     let prefix = "0".repeat(prefix_size);
+    let start = 1 << prefix_size;
     (start..).into_iter().find(|i| {
         let key = format!("{input}{i}");
         let digest = md5::compute(key.as_bytes());
