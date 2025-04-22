2b4989fb556eb6e788600c22cd13f799a6d15b47
Alex Morton <mortonar01@gmail.com>
2025-04-22 08:47:43 -0400

    2015 Day 6 Part 2

--- 2015/day6/src/main.rs
+++ 2015/day6/src/main.rs
@@ -15,8 +15,10 @@ fn main() -> Result<()> {
             &["turn", on_off] => {
                 let action = if on_off == "on" {
                     Action::On
-                } else {
+                } else if on_off == "off" {
                     Action::Off
+                } else {
+                    return Err(anyhow!("Unknown action: {on_off}"));
                 };
                 (action, parse_range(tokens[2])?, parse_range(tokens[4])?)
             }
@@ -25,11 +27,12 @@ fn main() -> Result<()> {
         lights.apply_action(&action, from, to);
     }
     println!("Part 1: {}", lights.lit_count());
+    println!("Part 2: {}", lights.total_brightness());
 
     Ok(())
 }
 
-fn parse_range(range: &str) -> Result<Range> {
+fn parse_range(range: &str) -> Result<Point> {
     let tokens: Vec<&str> = range.split(",").collect();
     if tokens.len() != 2 {
         Err(anyhow!("Invalid range: {}", range))
@@ -46,31 +49,44 @@ enum Action {
 }
 
 impl Action {
-    fn apply(&self, light: &mut bool) {
-        *light = match self {
-            Action::On => true,
-            Action::Off => false,
-            Action::Toggle => !*light,
+    fn apply(&self, light: &mut Light) {
+        match self {
+            Action::On => {
+                light.0 = true;
+                light.1 += 1;
+            }
+            Action::Off => {
+                light.0 = true;
+                if light.1 != 0 {
+                    light.1 -= 1;
+                };
+            }
+            Action::Toggle => {
+                light.0 = !light.0;
+                light.1 += 2;
+            }
         }
     }
 }
 
-type Range = (usize, usize);
+type Point = (usize, usize);
+
+type Light = (bool, usize);
 
 struct Lights {
-    lights: Vec<Vec<bool>>,
+    lights: Vec<Vec<Light>>,
 }
 
 impl Default for Lights {
     fn default() -> Self {
         Self {
-            lights: vec![vec![false; 1000]; 1000],
+            lights: vec![vec![(false, 0); 1000]; 1000],
         }
     }
 }
 
 impl Lights {
-    fn apply_action(&mut self, action: &Action, from: Range, to: Range) {
+    fn apply_action(&mut self, action: &Action, from: Point, to: Point) {
         for i in from.0..=to.0 {
             for j in from.1..=to.1 {
                 action.apply(&mut self.lights[i][j]);
@@ -81,7 +97,14 @@ impl Lights {
     fn lit_count(&self) -> usize {
         self.lights
             .iter()
-            .map(|row| row.iter().filter(|&&l| l == true).count())
+            .map(|row| row.iter().filter(|&&l| l.0 == true).count())
+            .sum()
+    }
+
+    fn total_brightness(&self) -> usize {
+        self.lights
+            .iter()
+            .map(|row| row.iter().map(|&l| l.1).sum::<usize>())
             .sum()
     }
 }
