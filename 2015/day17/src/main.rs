aa833b10f43336cd1e5cd3cadabdc7fe5af08990
Alex Morton <mortonar01@gmail.com>
2025-06-02 21:23:16 -0400

    Small optimization to 2015 Day 17
    We actually don't need to track the full selection of elements in a
    Vector at all. We enumerate them so we can only pick them distinctly.
    Also, only the total # selected and the sum actually matter.
    
    Shaves another ~0.1s off the time.

--- 2015/day17/src/main.rs
+++ 2015/day17/src/main.rs
@@ -14,26 +14,25 @@ fn main() -> Result<()> {
         containers
     };
 
-    let mut selection = Vec::with_capacity(containers.len());
     // Selection length -> # of ways to build a selection of that length
     let mut combinations = HashMap::new();
     // mask @ bit i == 1 -> choosing containers[i]
     for mask in 1..=(1 << containers.len()) {
         // Calculate what containers are selected with this mask
-        containers
+        let (num_selected, sum) = containers
             .iter()
             .enumerate()
             .filter(|(i, _c)| mask & (1 << *i) > 0)
-            .for_each(|(_i, c)| selection.push(*c));
+            .fold((0, 0), |(num_selected, sum), (_i, c)| {
+                (num_selected + 1, sum + *c)
+            });
 
-        if selection.iter().sum::<usize>() == to_store {
+        if sum == to_store {
             combinations
-                .entry(selection.len())
+                .entry(num_selected)
                 .and_modify(|c| *c += 1)
                 .or_insert(1);
         }
-
-        selection.clear();
     }
 
     println!("Part 1: {}", combinations.values().sum::<usize>());
