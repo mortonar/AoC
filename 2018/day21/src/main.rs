6196634ba91119298719918051791df6e777025a
Alex Morton <mortonar01@gmail.com>
2026-04-06 09:33:59 -0400

    [WIP] 2018 Day 21

---
+++ 2018/day21/src/main.rs
@@ -0,0 +1,8 @@
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    // Needed help with reverse engineering...
+    println!("Part 1: {}", 13270004);
+    println!("Part 2: {}", 12879142);
+    Ok(())
+}
