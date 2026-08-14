8b0c3a4c3f7d5bf2b0e5612c9471fe095fd0adb9
Alex Morton <mortonar01@gmail.com>
2026-08-14 09:08:23 -0400

    2021 Day 10

---
+++ 2021/day10/src/main.rs
@@ -0,0 +1,77 @@
+use anyhow::{Error, Result};
+use std::io::{BufRead, stdin};
+
+fn main() -> Result<()> {
+    let nav = parse_input()?;
+
+    println!("Part 1: {}", part1(&nav));
+    println!("Part 2: {}", part2(&nav));
+
+    Ok(())
+}
+
+fn parse_input() -> Result<Vec<String>> {
+    stdin()
+        .lock()
+        .lines()
+        .map(|line| line.map_err(Error::from))
+        .collect()
+}
+
+fn part1(nav: &[String]) -> usize {
+    nav.iter()
+        .map(|n| match syntax_eval(n) {
+            Ok(_) => 0,
+            Err(score) => score,
+        })
+        .sum()
+}
+
+fn part2(nav: &[String]) -> usize {
+    let mut scores: Vec<_> = nav
+        .iter()
+        .filter_map(|n| syntax_eval(n).ok())
+        .map(|stack| close_score(&stack))
+        .collect();
+    scores.sort();
+    scores[scores.len() / 2]
+}
+
+/// Return either OK with remaining stack or error with syntax error score
+fn syntax_eval(n: &str) -> Result<Vec<char>, usize> {
+    let mut stack = Vec::new();
+    for c in n.trim().chars() {
+        match c {
+            '(' | '{' | '[' | '<' => stack.push(c),
+            close => {
+                let open = stack.pop().unwrap_or('x');
+                let s = match (open, close) {
+                    ('(', ')') | ('[', ']') | ('{', '}') | ('<', '>') => 0,
+                    (_, ')') => 3,
+                    (_, ']') => 57,
+                    (_, '}') => 1197,
+                    (_, '>') => 25137,
+                    _ => panic!("Unhandled case: ({open}, {close})"),
+                };
+                if s != 0 {
+                    return Err(s);
+                }
+            }
+        }
+    }
+
+    Ok(stack)
+}
+
+fn close_score(stack: &[char]) -> usize {
+    stack.iter().rev().fold(0, |score, open| {
+        score * 5
+            + match open {
+                '(' => 1,
+                '[' => 2,
+                '{' => 3,
+                '<' => 4,
+                _ => panic!("Unhandled open: {open}"),
+            }
+    })
+}
