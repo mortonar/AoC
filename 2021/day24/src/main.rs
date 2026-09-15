a09647f5a279c850ed1326531d39fd2c0fa8acd0
Alex Morton <mortonar01@gmail.com>
2026-09-15 08:54:17 -0400

    2021 Day 24

---
+++ 2021/day24/src/main.rs
@@ -0,0 +1,8 @@
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    // AI-assisted reverse engineering :)
+    println!("Part 1: {}", 96979989692495u64);
+    println!("Part 2: {}", 51316214181141u64);
+    Ok(())
+}
